03b1b46812b1aeb48ea87bd80ac552b54a11b4d8
mr-ramu <raimugipan8769@gmail.com>
2024-07-15 00:18:24 +0900

    add view

--- bookshelfproject/BookshelfApp/views.py
+++ bookshelfproject/BookshelfApp/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.base import (
+  View, 
+)
+from . import forms
+from .models import User
 
-# Create your views here.
+
+
+class SignupView(View):
+  def get(self, request, *args, **kwargs):
+    signupform = forms.SignupForm()
+    return render(request, 'signup.html', context={
+      'signupform' : signupform,
+    })
+    
+  def post(self, request, *args, **kwargs):
+    signupform = forms.SignupForm(request.POST)
+    if signupform.is_valid():
+      signupform.save()
+      return redirect('BookshelfApp:login')
+    return render(request, 'signup.html', context={
+      'signupform' : signupform,
+    })
+
+
+class LoginView(View):
+  def get(self, request, *args, **kwargs):
+    loginform  = forms.LoginForm()
+    return render(request, 'login.html', context={
+      'loginform' : loginform,
+    })
+    
+  def post(self, request, *args, **kwargs):
+    loginform = forms.LoginForm(request.POST)
+    if loginform.is_valid():
+      loginform.save()
+      return redirect('BookshelfApp:signup')
+    return render(request, 'login.html', context={
+      'loginform' : loginform,
+    })
