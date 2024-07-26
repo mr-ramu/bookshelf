3bfb27127b8abaaff518809faa05a33eb9a88bc5
mr-ramu <raimugipan8769@gmail.com>
2024-07-26 22:40:12 +0900

    fix signupform and loginform

--- bookshelfproject/bookshelf/forms.py
+++ bookshelfproject/bookshelf/forms.py
@@ -1,7 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from django.contrib.auth import get_user_model
-from django.core.exceptions import ValidationError
 from .models import User
 import re
 from django.contrib.auth import authenticate
@@ -10,62 +8,50 @@ from django.contrib.auth import authenticate
 User = get_user_model()
 
 class SignupForm(forms.ModelForm):
-  confirm_password = forms.CharField(label='パスワード再入力', widget=forms.PasswordInput)
+  confirm_password = forms.CharField(label='パスワード再入力')
   
   class Meta:
     model = User
     fields = ('name', 'email', 'password')
     labels = {'name':'ユーザー名', 'email':'メールアドレス','password':'パスワード'}
-    widgets = {'password':forms.PasswordInput}
    
   def clean(self):
       cleaned_data = super().clean()
       password = cleaned_data.get('password')
       confirm_password = cleaned_data.get('confirm_password')
       if password != confirm_password:
-        raise ValidationError('パスワードが一致しません。')
+        self.add_error('confirm_password', 'パスワードが一致しません。')
       return cleaned_data
       
   def clean_password(self):
     password = self.cleaned_data.get('password')
-
     if password:
       if len(password) < 10:
-        raise ValidationError('10文字以上で入力してください。')
+        self.add_error('password','10文字以上で入力してください。')
       
       if not re.match('^[0-9a-zA-Z]*$', password):
-        raise ValidationError('パスワードは半角英数で入力してください。')
-
+        self.add_error('password', 'パスワードは半角英数で入力してください。')
     return password  
-
 
   def save(self, commit=True):
     name = self.cleaned_data['name']
     email = self.cleaned_data['email']
-    
     form = User(name=name, email=email)
     form = super().save(commit=False)
     form.set_password(self.cleaned_data['password'])
     if commit:
       form.save()
     
-    
-
-class LoginForm(forms.ModelForm):
-  class Meta:
-    model = User
-    fields = ('email', 'password')
-    labels = {'email':'メールアドレス','password':'パスワード'}
-    widgets = {'password':forms.PasswordInput}
-    
-  def clean(self):
-      cleaned_data = super().clean()
-      email = cleaned_data.get('email')
-      password = cleaned_data.get('password')
+class LoginForm(forms.Form):
+  email = forms.EmailField(max_length=254)
+  password = forms.CharField(max_length=64)
+  
+  def clean_password(self):
+    password = self.cleaned_data.get('password')
+    if password:
+      if len(password) < 10:
+        self.add_error('password', '10文字以上で入力してください。')
       
-      if email and password:
-        is_valid = authenticate(email=email, password=password)
-        
-      if not is_valid:
-        raise ValidationError('メールアドレスまたはパスワードが違います。')
-      return     
+      if not re.match('^[0-9a-zA-Z]*$', password):
+        self.add_error('password', 'パスワードは半角英数で入力してください。')
+    return password
